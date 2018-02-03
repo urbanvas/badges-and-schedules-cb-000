a7df40da880b80b084977652c4ff745d450ba67b
Neil Seejoor <nvseejoor@gmail.com>
2018-02-03 22:23:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(attendees)
+  counter = 0
+  attendees.map {|attendees| "Hello, #{attendees}! You'll be assigned to room #{counter += 1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each {|items| puts items}
+  assign_rooms(attendees).each {|person| puts person }
+end
